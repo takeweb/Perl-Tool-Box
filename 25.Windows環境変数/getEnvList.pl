f95307ee226e39fb20c6d49bc79446449bf4a554
taketomo ohishi <takeweb@mac.com>
2012-09-07 16:53:25 +0900

    No.25を追加

---
+++ 25.Windows環境変数/getEnvList.pl
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+use Win32::OLE;
+
+# 変数宣言
+my $WshShell = Win32::OLE->CreateObject("WScript.Shell");
+my @SysEnv = $WshShell->Enviroment("SYSTEM");
+my @UsrEnv = $WshShell->Enviroment("USER");
+
+foreach my $value (@SysEnv) {
+	print "システム環境変数：$value\n";	
+}
+
+foreach my $value (@UsrEnv) {
+	print "ユーザ環境変数：$value\n";	
+}
+
