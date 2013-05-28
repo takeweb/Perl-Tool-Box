458575c57185919ab0908c4d2b1835b0333270be
taketomo ooishi <takeweb@mac.com>
2013-05-28 20:46:43 +0900

    掃除機

--- 32.掃除機/cleaner.pl
+++ 32.掃除機/cleaner.pl
@@ -1,8 +1,24 @@
 use strict;
 use warnings;
+use strict;
+use warnings;
+use File::Path 'rmtree';
 
-unlink qq{D:\ooishi\tmp};
-unlink qq{C:\Users\taketomoooishi\Downloads};
-unlink qq{C:\$Recycle.Bin};
-unlink qq{D:\$RECYCLE.BIN};
+#unlink qq{D:\ooishi\tmp};
+#unlink qq{C:\Users\taketomoooishi\Downloads};
+#unlink qq{C:\$Recycle.Bin};
+#unlink qq{D:\$RECYCLE.BIN};
+
+my @list;
+my @list1 = glob(q{D:\ooishi\tmp\*});
+my @list2 = glob(q{C:\Users\taketomoooishi\Downloads\*});
+
+push(@list, @list1);
+push(@list, @list2);
+
+foreach my $file (@list) {
+    print "$file\n";
+}
+
+rmtree(@list,1,1);
 
